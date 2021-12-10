b3c6d30b157b52427ba216948cb8f1f70ab393e1
surbhivarande <80210499+surbhivarande@users.noreply.github.com>
2021-12-10 09:18:24 +0530

    Update OnlineproductshoppingApplication.java

--- Onlineproductshopping/java/com/product/onlineproductshopping/OnlineproductshoppingApplication.java
+++ Onlineproductshopping/java/com/product/onlineproductshopping/OnlineproductshoppingApplication.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineproductshoppingApplication {
 
 	public static void main(String[] args) {
+		try {
 		SpringApplication.run(OnlineproductshoppingApplication.class, args);
 	}
+		catch(Exception e) {
+			System.out.println(e);
+		    System.out.println("Oops!Sorry,Something went to wrong");	
+		}
+		finally {
+		System.out.println("An error has occured requested page not found!");	
+		}
+		}
 
 }
